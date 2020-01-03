99359d2e3504cb90872f349d662c68107c83d58b
Saurabh Machave <unknown7bolt@gmail.com>
2020-01-03 20:42:22 +0530

    initial commit

---
+++ src/build_cop_bot/core.clj
@@ -0,0 +1,7 @@
+(ns build-cop-bot.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/build_cop_bot/core_test.clj
@@ -0,0 +1,7 @@
+(ns build-cop-bot.core-test
+  (:require [clojure.test :refer :all]
+            [build-cop-bot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
